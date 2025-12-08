7bac6f46bcde3a04ab89052b2e8d3ec3e7ff12c3
monaboiste <monikaszumna4@gmail.com>
2025-12-08 18:57:54 +0100

    feat(node): install lts

--- .config/zsh/devtool.zsh
+++ .config/zsh/devtool.zsh
@@ -1,11 +1,19 @@
 #!/usr/bin/zsh
 
-export SDKMAN_DIR="$XDG_BIN_HOME"/sdkman
+# SDKMAN
+#
+export SDKMAN_DIR="${XDG_BIN_HOME:-$HOME/.sdkman}/sdkman"
 if [[ ! -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]]; then
   curl -s "https://get.sdkman.io" | bash
 fi
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
-
-export NVM_DIR="$XDG_BIN_HOME"/nvm
+# Node
+#
+export NVM_DIR="${XDG_BIN_HOME:-$HOME/.nvm}/nvm"
 source $(brew --prefix nvm)/nvm.sh
+
+if ! (( $+commands[npm] )); then
+  echo "Installing latest Node.js LTS via nvm..."
+  nvm install --lts >/dev/null
+fi
