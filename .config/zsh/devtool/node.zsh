98aef799fcd4ddd43ab3f48772d97371c063b6f8
monaboiste <monikaszumna4@gmail.com>
2025-12-09 21:05:25 +0100

    chore(node)!: replace nvm with fnm

--- .config/zsh/devtool/node.zsh
+++ .config/zsh/devtool/node.zsh
@@ -1,20 +1,11 @@
 #!/usr/bin/zsh
 
-export NVM_DIR="${XDG_BIN_HOME:-$HOME/.nvm}/nvm"
+export FNM_DIR="${XDG_BIN_HOME:-$HOME/.local/bin}/fnm"
 
-_load_nvm() {
-  unset -f nvm npm npx node _load_nvm
-  source "$(brew --prefix nvm)/nvm.sh"
-}
+eval "$(fnm env --use-on-cd)"
 
-nvm()  { _load_nvm; nvm  "$@"; }
-npm()  { _load_nvm; npm  "$@"; }
-npx()  { _load_nvm; npx  "$@"; }
-node() { _load_nvm; node "$@"; }
-
-local node_versions=("$NVM_DIR"/versions/node/*(N))
+local node_versions=("$FNM_DIR"/node-versions/*(N))
 if (( ${#node_versions} == 0 )); then
-  echo "Installing latest Node.js LTS via nvm..."
-  _load_nvm
-  nvm install --lts >/dev/null
+  echo "No Node.js versions found. Installing latest LTS..."
+  fnm install --lts
 fi
